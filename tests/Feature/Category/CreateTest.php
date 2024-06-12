0f2dfe79e7dcd3b81ee75e815cb9c375321d9ee8
Ronildo Sousa <sousaronildo400@gmail.com>
2024-06-12 10:53:58 -0300

    feat: tests for create a category

---
+++ tests/Feature/Category/CreateTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Category;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_should_be_able_to_create_a_category()
+    {
+        $owner = User::factory()->create();
+        $payload = [
+            'title' => fake()->sentence(3),
+            'description' => fake()->sentence(10),
+        ];
+
+        $response = $this->actingAs($owner)
+            ->post(route('categories.insert'), $payload);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+
+        $this->assertDatabaseHas('categories', array_merge(
+            $payload,
+            ['owner_id' => $owner->id]
+        ));
+    }
+
+    #[Test]
+    public function title_field_and_description_should_be_required()
+    {
+        $owner = User::factory()->create();
+        $payload = [
+            'title' => '',
+            'description' => '',
+        ];
+
+        $response = $this->actingAs($owner)
+            ->post(route('categories.insert'), $payload);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
