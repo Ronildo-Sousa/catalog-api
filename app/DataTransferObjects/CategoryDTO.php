af4c0112408dae264c38ac5be8ffdf48232f7377
Ronildo Sousa <sousaronildo400@gmail.com>
2024-06-13 18:53:22 -0300

    refactor: category dto

--- app/DataTransferObjects/CategoryDTO.php
+++ app/DataTransferObjects/CategoryDTO.php
@@ -2,21 +2,15 @@
 
 namespace App\DataTransferObjects;
 
-use App\Contracts\DTOInterface;
 use Illuminate\Http\Request;
 
-class CategoryDTO implements DTOInterface
+class CategoryDTO extends AbstractDTO
 {
-    public readonly ?int $id;
-
     public function __construct(
-        $id = null,
         public readonly ?string $title,
         public readonly ?string $description,
         public readonly ?string $owner_id,
-    ){
-        $this->id = $id;   
-    }
+    ) {}
 
     public static function from(array|Request $data): self
     {
@@ -24,18 +18,10 @@ class CategoryDTO implements DTOInterface
             $data = $data->toArray();
         }
 
-        return new CategoryDTO(
-            id: data_get($data, 'id'),
+        return new self(
             title: data_get($data, 'title'),
             description: data_get($data, 'description'),
             owner_id: data_get($data, 'owner_id')
         );
     }
-
-    public function toArray(): array
-    {
-        return (array)$this;
-    }
 }
-
-
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\DataTransferObjects\CategoryDTO;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use App\Models\User;
 
@@ -12,9 +13,11 @@ class CategoryService
 
     public function list(int $per_page = self::CATEGORIES_PER_PAGE)
     {
-        return Category::query()
-            ->where('owner_id', '=', auth()->id())
-            ->paginate($per_page);
+        return CategoryResource::collection(
+            Category::query()
+                ->where('owner_id', '=', auth()->id())
+                ->paginate($per_page)
+        );
     }
 
     public function create(array $paylod): CategoryDTO
@@ -32,7 +35,7 @@ class CategoryService
     {
         return CategoryDTO::from($category->toArray());
     }
-    
+
     public function update(array $paylod, Category $category): ?CategoryDTO
     {
         /** @var User $user */
@@ -40,7 +43,7 @@ class CategoryService
         if ($user->cannot('update', $category)) {
             return null;
         }
-        
+
         $category->update($paylod);
         return CategoryDTO::from($category->toArray());
     }
